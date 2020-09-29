25aab85693fc6240eff24c3b134ef42106e40e9c
Harry <hs3061@columbia.edu>
2020-09-28 20:17:42 -0400

    js-stuff

---
+++ js_sketches/heimdal/sketch.js
@@ -0,0 +1,17 @@
+// const baseRadius = 0.1
+// const scaleFactor = Math.SQRT2;
+
+// function oneTriangle() {
+// 	translate()
+// }
+
+var matrix = {'A' : []}
+
+function setup() {
+	createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+	scale(10)
+	ellipse(10, 10, 20)
+}
